a85452f552e949f39f93ce6035e80f203f92e6e0
Tiia Jauhiainen <tiia.jauhiainen@edu.sasky.fi>
2023-02-20 10:02:01 +0200

    yksittäisen tapahtuman tietojen tulostaminen

--- src/model/tapahtuma.php
+++ src/model/tapahtuma.php
@@ -6,4 +6,8 @@
     return DB::run('SELECT * FROM koira ORDER BY tap_alkaa;')->fetchAll();
   }
 
+  function haeTapahtuma($id) {
+    return DB::run('SELECT * FROM koira WHERE idtapahtuma = ?;',[$id])->fetch();
+  }
+
 ?>
