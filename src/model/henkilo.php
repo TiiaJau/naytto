59cbcfd15251f71071a49542f086c17580e19ac2
Tiia Jauhiainen <tiia.jauhiainen@edu.sasky.fi>
2023-03-03 09:27:02 +0200

    vaihtoavaimen lisääminen henkilo-tauluun

--- src/model/henkilo.php
+++ src/model/henkilo.php
@@ -22,4 +22,16 @@
   function vahvistaTili($avain) {
     return DB::run('UPDATE omistaja SET vahvistettu = TRUE WHERE vahvavain = ?', [$avain])->rowCount();
   }
+
+  function asetaVaihtoavain($email,$avain) {
+    return DB::run('UPDATE henkilo SET nollausavain = ?, nollausaika = NOW() + INTERVAL 30 MINUTE WHERE email = ?', [$avain,$email])->rowCount();
+  }
+
+  function tarkistaVaihtoavain($avain) {
+    return DB::run('SELECT nollausavain, nollausaika-NOW() AS aikaikkuna FROM omistaja WHERE nollausavain = ?', [$avain])->fetch();
+  }
+
+  function vaihdaSalasanaAvaimella($salasana,$avain) {
+    return DB::run('UPDATE omistaja SET salasana = ?, nollausavain = NULL, nollausaika = NULL WHERE nollausavain = ?', [$salasana,$avain])->rowCount();
+  }
 ?>
