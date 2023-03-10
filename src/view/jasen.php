248f24812816c33fad681fcb73c7a4398b77b3fa
Tiia Jauhiainen <tiia.jauhiainen@edu.sasky.fi>
2023-03-10 09:14:09 +0200

    muokkaa jäsen-sivun tekstin ulkonäköä

--- src/view/jasen.php
+++ src/view/jasen.php
@@ -5,8 +5,9 @@
       <img src="images/martta2.png" alt="">  </div> 
 <p>Voit liittyä Teerivaaran koiraharrastajien jäseneksi<br>
 ilmoittautumalla jäsensihteerillemme : <br><br>
-jasentvkoira@sahkoposti.fi<br><br><br><br><br><br></p>
+jasentvkoira@sahkoposti.fi<br><br><br><br></p>
 <h3>Jäsenmaksut 2023</h3>
-<p>Varsinainen jäsen  20,00 eur<br>
-Perhejäsen 5,00 eur<br>
-Liittymismaksu 5,00 eur</p>
+<p><pre>
+Varsinainen jäsen       20,00 eur<br>
+Perhejäsen              5,00 eur<br>
+Liittymismaksu          5,00 eur</pre></p>
