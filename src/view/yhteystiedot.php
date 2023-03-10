0eddaaf4ddfd4da5612b461e618d28f1daca8b41
Tiia Jauhiainen <tiia.jauhiainen@edu.sasky.fi>
2023-03-10 09:34:21 +0200

    lisää kuvan yhteystiedot-sivulle

--- src/view/yhteystiedot.php
+++ src/view/yhteystiedot.php
@@ -1,9 +1,12 @@
 <?php $this->layout('template', ['title' => 'Yhteystiedot']) ?>
 
 <h1>Yhteystiedot</h1>
+<div class="kuva kuva--oikea">       
+      <img src="images/malikka.jpg" alt="">  </div> 
 <p>Teerivaaran koiraharrastajat Ry<br>
 Teerivaarantie 91<br>
 83700 POLVIJÄRVI<br><br>
 Puhelinnumero : 050 123 4567<br><br>
 Sähköposti : tvkoira@sahkoposti.fi<br><br>
-Yhteyshenkilö : Tiia Jauhiainen</p>
+Yhteyshenkilö : Tiia Jauhiainen
+<br><br><br><br><br><br><br><br><br><br><br></p>
